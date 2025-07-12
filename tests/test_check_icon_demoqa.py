0d684583ac45a3e063a5c9c3bbd959b8f88e9412
TiDaBo <illinois2007@gmail.com>
2025-07-13 02:21:10 +0300

    first commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,12 +1,29 @@
+# from selenium.webdriver.common.by import By
+#
+# def test_check_icon(browser):
+#     browser.get("https://demoqa.com/")
+#
+#     icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
+#
+#     if icon is None:
+#         print('Элемент не найден')
+#     else:
+#         print('Элемент найден')
 
-from selenium.webdriver.common.by import By
+
+from pages.demoqa import DemoQA
+# import time #time.sleep(2)
 
 def test_check_icon(browser):
-    browser.get("https://demoqa.com/")
 
-    icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
+    demo_qa_page = DemoQA(browser)
+    demo_qa_page.visit()
+    demo_qa_page.click_on_the_icon()
+    assert demo_qa_page.equal_url()
+    assert demo_qa_page.exist_icon()
 
-    if icon is None:
-        print('Элемент не найден')
-    else:
-        print('Элемент найден')
+# def click_icon(browser):
+#
+#     demo_qa_page = DemoQA(browser)
+#     demo_qa_page.visit()
+#     demo_qa_page.click_on_the_icon()
