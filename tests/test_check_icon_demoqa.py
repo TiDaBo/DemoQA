a7e9a63dcfc3494f1d215d9ec974e7c2a3cc2b91
TiDaBo <illinois2007@gmail.com>
2025-07-13 02:23:16 +0300

    first commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -21,9 +21,3 @@ def test_check_icon(browser):
     demo_qa_page.click_on_the_icon()
     assert demo_qa_page.equal_url()
     assert demo_qa_page.exist_icon()
-
-# def click_icon(browser):
-#
-#     demo_qa_page = DemoQA(browser)
-#     demo_qa_page.visit()
-#     demo_qa_page.click_on_the_icon()
