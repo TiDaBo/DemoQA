60203940c9f572bae4d7a087e1730c6bfefcfe0a
TiDaBo <illinois2007@gmail.com>
2025-07-15 19:42:06 +0300

    first commit

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -7,4 +7,9 @@ class ElementsPage(BasePage):
         self.base_url = 'https://demoqa.com/elements'
         super().__init__(driver, self.base_url)
 
-        self.center_area = WebElement(driver, '*.col-md-6')
+        self.center_area = WebElement(driver, '.col-md-6')
+        self.text_elements = WebElement(driver, 'div.col-12:nth-child(2)')
+        self.icon = WebElement(driver, 'header > a > img')
+        self.btn_sidebar_first = WebElement(driver, 'div:nth-child(1) > span > div')
+        self.btn_sidebar_first_textbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-0 > span')
+        self.btn_sidebar_first_checkbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-1 > span')
--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -5,7 +5,6 @@
 from pages.demoqa import DemoQA
 
 def test_check_text(browser):
-
     demo_qa_page = DemoQA(browser)
     demo_qa_page.visit()
     assert demo_qa_page.footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
@@ -18,11 +17,19 @@ def test_check_text(browser):
 from pages.elements_page import ElementsPage
 
 def test_check_text_elements(browser):
-
     demo_qa_page = DemoQA(browser)
-    elements_page = ElementsPage(browser)
+    el_page = ElementsPage(browser)
 
     demo_qa_page.visit()
     demo_qa_page.btn_elements.click()
-    assert elements_page.center_area.get_text() == 'Please select an item from left to start practice.'
+    assert el_page.center_area.get_text() == 'Please select an item from left to start practice.'
+
+
+def test_page_elements(browser):
+    el_page = ElementsPage(browser)
+    el_page.visit()
+
+    assert el_page.icon.exist()
+    assert el_page.btn_sidebar_first.exist()
+    assert el_page.btn_sidebar_first_textbox.exist()
 
